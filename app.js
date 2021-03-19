1c889ea269e0b336a789a03284b8039bece6340d
lukegammon <54638816+lukegammon@users.noreply.github.com>
2021-03-19 18:04:05 +0000

    last

--- app.js
+++ app.js
@@ -7,14 +7,67 @@ document.addEventListener( 'DOMContentLoaded', function () {
         'arrows': 'slider',
         'pagination': false,
     } ).mount();
+
     // Custom styling of arrows (splide js only allows API style changing of buttons etc.)
     const arrows = splide.root.children[0];
     const nextArrow = splide.Components.Arrows.arrows.next;
     const prevArrow = splide.Components.Arrows.arrows.prev;
-    arrows.style.position = "absolute";
-    arrows.style.bottom = "1.5rem";
-    arrows.style.right = "-0.85rem";
-    arrows.style.width = "9.6rem";
+    const image1 = document.querySelectorAll(".slide-img1");
+    const image2 = document.querySelectorAll(".slide-img2");
+    const image3 = document.querySelectorAll(".slide-img3");
+
+    
+    
+    console.log(image1[0].parentElement.style.background);
+    const setDesktop = () => {
+        arrows.style.position = "absolute";
+        arrows.style.bottom = "1.5rem";
+        arrows.style.right = "-8.7rem";
+        arrows.style.width = "9.6rem";
+        image1.forEach(image => {
+            image.parentElement.style.background = "url('./images/desktop-image-hero-1.jpg')";
+            image.parentElement.style.backgroundSize = "cover";
+        })
+        image2.forEach(image => {
+            image.parentElement.style.background = "url('./images/desktop-image-hero-2.jpg')";
+            image.parentElement.style.backgroundSize = "cover";
+        })
+        image3.forEach(image => {
+            image.parentElement.style.background = "url('./images/desktop-image-hero-3.jpg')";
+            image.parentElement.style.backgroundSize = "cover";
+        })
+    }
+    const setMobile = () => {
+        arrows.style.position = "absolute";
+        arrows.style.bottom = "1.5rem";
+        arrows.style.right = "-0.85rem";
+        arrows.style.width = "9.6rem";
+        image1.forEach(image => {
+            image.parentElement.style.background = "url('./images/mobile-image-hero-1.jpg')";
+            image.parentElement.style.backgroundSize = "cover";
+        })
+        image2.forEach(image => {
+            image.parentElement.style.background = "url('./images/mobile-image-hero-2.jpg')";
+            image.parentElement.style.backgroundSize = "cover";
+        })
+        image3.forEach(image => {
+            image.parentElement.style.background = "url('./images/mobile-image-hero-3.jpg')";
+            image.parentElement.style.backgroundSize = "cover";
+        })
+    }
+
+    if(window.innerWidth >= 1030){
+        setDesktop();
+    } else {
+        setMobile();
+    }
+    window.addEventListener('resize', (e) => {
+        if(window.innerWidth >= 1030){
+            setDesktop();
+        } else {
+            setMobile();
+        }
+    });
     nextArrow.style.background = "black";
     prevArrow.style.background = "black";
     nextArrow.style.opacity = "1";
@@ -75,7 +128,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
             focusDescription.innerHTML = "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.";
         } else if(e.index === 1) {
             focusTitle.innerHTML = "We are available all across the globe"
-            focusDescription.innerHTML = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.";
+            focusDescription.innerHTML = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today. We Always have time to chat with you.";
         } else {
             focusTitle.innerHTML = "Manufactured with the best materials"
             focusDescription.innerHTML = "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want.";
